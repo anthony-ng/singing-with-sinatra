38f067fa549aa263d3154dbad54b6f575dfca358
anthony-ng <thomsonng123@gmail.com>
2014-12-10 20:10:52 -0800

    added edit & delete features

--- recall.rb
+++ recall.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'datamapper'
+require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
 
@@ -12,10 +12,44 @@ class Note
 	property :updated_at, DateTime
 end
 
-DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	@notes = Note.all :.order => :id.desc
-	@title = 'All Notes'
-	erb :home
+  @notes = Note.all :order => :id.desc
+  @title = 'All Notes'
+  erb :home
 end
+
+##### Adding a Note to the Database
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+##### Editing a Note
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+##### Using PUT for Editing
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.save
+  redirect '/'
+end
+
+##### Deleting a Note
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of ntoe ##{params[:id]}"
+  erb :delete
+end
+
+DataMapper.finalize.auto_upgrade!
